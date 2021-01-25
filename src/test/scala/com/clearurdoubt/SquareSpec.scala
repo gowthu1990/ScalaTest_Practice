4d4cba47b881276e7b5bb79443e49d8bc3cc670f
gowth <gowtam.badvity@gmail.com>
2021-01-25 20:54:36 +0000

    Initial

--- src/test/scala/com/clearurdoubt/SquareSpec.scala
+++ src/test/scala/com/clearurdoubt/SquareSpec.scala
@@ -1,21 +1,19 @@
 package com.clearurdoubt
 
-import org.scalatest
 import org.scalatest.featurespec.AnyFeatureSpec
 import org.scalatest.matchers.should.Matchers
 
-object SquareSpec extends AnyFeatureSpec with Matchers {
+class SquareSpec extends AnyFeatureSpec with Matchers {
+   Feature("SquareSpec") {
+     Scenario("A Square should fail if the side length is negative") {
+       assertThrows[java.lang.IllegalArgumentException] {
+         Square(-5)
+       }
+     }
 
-  println("Starting: ")
-
-  Scenario("A Square should fail if the side length is negative") {
-    assertThrows[java.lang.IllegalArgumentException] {
-      Square(-5)
-    }
-  }
-
-  Scenario("Correct Square area needs to be returned") {
-    val square = Square(5.0)
-    assert(square.area == 25.0)
-  }
+     Scenario("Correct Square area needs to be returned") {
+       val square = Square(5.0)
+       assert(square.area == 25.0)
+     }
+   }
 }
