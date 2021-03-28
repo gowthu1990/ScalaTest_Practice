870db25c23f269e94d139151bdfe8feae4e49901
gowth <gowtam.badvity@gmail.com>
2021-03-28 14:26:50 +0100

    Bubble Sort

---
+++ src/test/scala/com/clearurdoubt/practice/sorting/SortSpec.scala
@@ -0,0 +1,8 @@
+package com.clearurdoubt.practice.sorting
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class SortSpec extends AnyFlatSpec with Matchers {
+
+}
